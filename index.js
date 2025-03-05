f1bb5f63033771d42e33fe95d9c6bf4ebeb970f8
Mocha Gabriel <mochagabriel49@gmail.com>
2025-03-05 05:16:19 +0300

    Initial commit for backend

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Routes
 app.use('/api/user', userRoutes);
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on http://0.0.0.0:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
